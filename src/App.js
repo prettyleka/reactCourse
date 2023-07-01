b642f3e344c7e83083208055dd6153efcb56bc32
Valeria Basov <prettyleka@gmail.com>
2023-07-01 16:25:15 +0300

    try

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import NucampLogo from './app/assets/img/logo.png';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Hello nucamp
-    </div>
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand href='/'>
+            <img src={NucampLogo} alt='nucamp logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar> 
+      I'm ready for workshop!
+   </div>
   );
 }
 
