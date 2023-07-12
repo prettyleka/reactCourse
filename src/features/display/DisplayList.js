e3e2e3fa922be17967b5ee0ef15b68b915cb81fe
Valeria Basov <prettyleka@gmail.com>
2023-07-12 21:07:22 +0300

    animated

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,6 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+//import DisplayCard from './DisplayCard';
+import  AnimatedDisplayCard  from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner }from '../partners/partnersSlice';
@@ -9,12 +10,13 @@ const DisplayList =()=>{
     return (
         <Row>
             {items.map((item, idx) => {
-                return( 
-                    <Col className='m-1' md key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
-                );
-            })}
+                  return (
+                    item && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={item} />
+                        </Col>
+                    )
+            )})}
         </Row>
     );
 }
