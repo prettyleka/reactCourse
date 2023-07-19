050067688d5003334f0132fe860f55b69c5d04f9
Valeria Basov <prettyleka@gmail.com>
2023-07-20 00:02:13 +0300

    middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 //import counterReducer from '../features/counter/counterSlice';
 import { campsitesReducer } from '../features/campsites/campsitesSlice'
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer} from '../features/promotions/promotionsSlice';
+import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
@@ -12,4 +13,5 @@ export const store = configureStore({
     partners: partnersReducer,
     promotions: promotionsReducer
   },
+  middleware: (getDefaultMiddleware) =>getDefaultMiddleware().concat([logger])
 });
