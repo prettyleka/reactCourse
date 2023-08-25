249429ca2e4b5e938c6c186e2ffd19fc4c6676ab
Valeria Basov <prettyleka@gmail.com>
2023-08-25 20:52:50 +0300

    gestures

--- 4-React-Native.nosync/nucampsite/features/campsites/RenderCampsite.js
+++ 4-React-Native.nosync/nucampsite/features/campsites/RenderCampsite.js
@@ -1,18 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable'
+import { useRef } from 'react';
 
 
 const RenderCampsite = (props) => {
 
     const { campsite } = props;
+    const view = useRef();
+    const isLeftSwipe =({ dx })=> dx < -200;
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if(isLeftSwipe(gestureState)){
+                Alert.alert(
+                    'Add Favorite',  
+                'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                [{
+                    text: 'Cancel',
+                    style: 'cancel',
+                    onPress: () => console.log('Cancel Pressed')
+                },
+                {
+                    text: 'OK',
+                    onPress: () =>
+                        props.isFavorite
+                            ? console.log('Already set as a favorite')
+                            : props.markFavorite()
+                }], 
+                { cancelable: false })
+            }
+        },
+        onPanResponderGrant: ()=>{
+            view.current.rubberBand(1000).then((endState) => console.log(endState.finished ? 'finished' : 'canceled'))
+        }
+    })
 
     if (campsite) {
         return (
             <Animatable.View animation='fadeInDownBig'
                     duration={2000}
-                    delay={1000}>
+                    delay={1000}
+                    //prop using the spread operator on panResponder.panHandlers.
+                    {...panResponder.panHandlers}
+                    ref={view}>
             <Card containerStyle={styles.cardContainer}>
                 <Card.Image source={{ uri: baseUrl + campsite.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
